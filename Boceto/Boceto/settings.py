dbf1cac31f6e1c989da6a3c1ccd744e0b45121ba
xtaxx12 <rojassebas765@gmail.com>
2023-07-01 19:59:14 -0500

    revisen

--- Boceto/Boceto/settings.py
+++ Boceto/Boceto/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'BocetoApp',
+    'Nosotros',
 ]
 
 MIDDLEWARE = [
---
+++ Boceto/Nosotros/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Nosotros(request):
+    return render(request,"Nosotros/noso.html")
+    
