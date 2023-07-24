461bfa5036655d3e6e86d240617bbc8e7f19aad7
xtaxx12 <rojassebas765@gmail.com>
2023-07-24 11:20:15 -0500

    listo

--- Boceto/BocetoApp/views.py
+++ Boceto/BocetoApp/views.py
@@ -18,11 +18,10 @@ def enviar_correo(request):
     if request.method == 'POST':
         email = request.POST.get('email', '')  # Obtenemos el correo electrónico ingresado por el usuario en home.html
          # Ahora enviamos el correo electrónico
-        subject = 'Gracias por ponerte en contacto con nosotros'  # Asunto del correo
-        message = f'Hola,\n\nGracias por ponerte en contacto con nosotros. Pronto te responderemos.\n\nAtentamente,\nEl equipo de Cooperativa Oro Verde'  # Contenido del mensaje
-        from_email = 'rojassebas765@gmail.com'  # Remitente del correo 
-        recipient_list = [email]  # Lista de destinatarios, en este caso, solo enviaremos el correo al email ingresado por el usuario
-        # Enviamos el correo
+        subject = 'Gracias por ponerte en contacto con nosotros'
+        message = f'Hola,\n\nGracias por ponerte en contacto con nosotros. Pronto te responderemos.\n\nAtentamente,\nEl equipo de Cooperativa Oro Verde'
+        from_email = email  # le mandamos la variable email para que envie el mensaje al correo ingresado por el usuario como remitente
+        recipient_list = [email]
         try:
             send_mail(subject, message, from_email, recipient_list)
             enviado_correctamente = True  # Actualizamos el valor de la variable a True si se envió correctamente
